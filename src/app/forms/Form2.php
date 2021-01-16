678d6a6d0c100d811599624c583baa93e6661e64
ArteM228a <58882426+ArteM228a@users.noreply.github.com>
2021-01-16 13:09:31 +0300

    123

---
+++ src/app/forms/Form2.php
@@ -0,0 +1,18 @@
+<?php
+namespace app\forms;
+
+use std, gui, framework, app;
+
+
+class Form2 extends AbstractForm
+{
+
+    /**
+     * @event show 
+     */
+    function doShow(UXWindowEvent $e = null)
+    {    
+        
+    }
+
+}
